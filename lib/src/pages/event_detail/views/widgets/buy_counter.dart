3fad398e62b2c60d7e1bcded7b2898ec9f508fdd
Ali Karimi <a.karimi2729@gmail.com>
2024-11-02 11:06:39 +0330

    added buy counter widget

---
+++ lib/src/pages/event_detail/views/widgets/buy_counter.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class BuyCounter extends StatefulWidget {
+  const BuyCounter({
+    super.key,
+    required this.maxValue,
+    required this.minValue,
+    required this.onChanged,
+  });
+
+  final int minValue;
+  final int maxValue;
+  final void Function(int) onChanged;
+
+  @override
+  State<BuyCounter> createState() => _BuyCounterState();
+}
+
+class _BuyCounterState extends State<BuyCounter> {
+  late int _currentValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentValue = widget.minValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: _decrement,
+          icon: const Icon(Icons.remove),
+        ),
+        const SizedBox(width: 12),
+        ColoredBox(
+          color: Colors.yellow,
+          child: Text('$_currentValue'),
+        ),
+        const SizedBox(width: 12),
+        IconButton(
+          onPressed: _increment,
+          icon: const Icon(Icons.add),
+        ),
+      ],
+    );
+  }
+
+  void _increment() {
+    setState(() {
+      if (_currentValue < widget.maxValue) {
+        _currentValue++;
+        widget.onChanged(_currentValue);
+      }
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      if (_currentValue > widget.minValue) {
+        _currentValue--;
+        widget.onChanged(_currentValue);
+      }
+    });
+  }
+}
